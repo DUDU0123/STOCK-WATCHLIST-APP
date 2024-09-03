61a4df984870aa056a7b63702c02eb530529ab7a
DUDU0123 <sdu200115@gmail.com>
2024-09-03 23:46:17 +0530

    Some minor updates

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,7 +1,1 @@
-import 'package:stock_watchlist/core/service_keys/service_keys.dart';
 String baseUrl = 'https://www.alphavantage.co/query?function=';
-String symbol = '';
-String askask =
-    'https://www.alphavantage.co/query?function=OVERVIEW&symbol=$symbol&apikey=$stockMarketApiKey';
-String sharePriceUrl =
-    'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=$symbol&apikey=$stockMarketApiKey';
